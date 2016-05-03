6582a6730026d873ef4dda65db49c8b2f57a4432
MOCHAMMAD DEARIFALDI AL IKHSAN <aksel.aldi@yahoo.co.id>
2016-05-04 00:15:29 +0700

    Source Code Tugas

---
+++ src/TimT.java
@@ -0,0 +1,13 @@
+
+public class TimT extends Member {
+
+    public TimT(String nama, String asal) {
+        super(nama, asal);
+
+    }
+
+    public String yel() {
+        return String.format("GO TEAM TI GO");
+
+    }
+}
