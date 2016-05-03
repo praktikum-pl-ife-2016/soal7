bd1b5feb73f8c86edbab6c95c188ab1c337571fe
RiskaAndriani <andrianiriska17.ar@gmail.com>
2016-05-03 19:58:26 +0700

    Pengumpulan Soal 7

---
+++ src/TimJ.java
@@ -0,0 +1,12 @@
+
+public class TimJ extends Member {
+    
+    @Override
+    public void yel(){
+        System.out.println("WASSHOI J!");
+    }
+        TimJ(String name, String asal){
+        super(name, asal);
+    }
+    
+}
