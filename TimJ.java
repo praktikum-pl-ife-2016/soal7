b741e47beaf955d41b175bc2c45434a5b85b74f6
jerryxsilent <erryend_sdm@yahoo.co.id>
2016-05-04 02:18:52 +0700

    src

---
+++ TimJ.java
@@ -0,0 +1,17 @@
+package bab7;
+
+/**
+ *
+ * @author JERRY
+ */
+ public class TimJ extends Member {
+
+    public TimJ(String nama, String daerah) {
+        super(nama, daerah);
+    }
+    
+    @Override
+    public void yel(){
+        System.out.println("WASSHOI J\n");
+    }    
+}
---
+++ TimT.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bab7;
+
+/**
+ *
+ * @author JERRY
+ */
+    public class TimT extends Member{
+
+    public TimT(String nama, String daerah) {
+        super(nama, daerah);
+    }
+    @Override
+    public void yel(){
+        System.out.println("GO TEAM TI GO\n");
+    } 
+}
