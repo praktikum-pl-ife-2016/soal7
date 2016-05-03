8e5618ef1d3d27ea4bb1a093cbba6e1ff49d80f9
Rizkyyud <rizkyyud27@gmail.com>
2016-05-03 23:33:20 +0700

    Tambah file dan edit data

---
+++ src/TimJ.java
@@ -0,0 +1,16 @@
+public class TimJ extends Member {
+
+    public TimJ(String nama, String daerah) {
+        super(nama, daerah);
+    }
+
+    @Override
+    public String yel() {
+        return String.format("WASSHOI J!");
+    }
+
+    @Override
+    public void show(){
+        super.show();
+    }
+}
---
+++ src/TimT.java
@@ -0,0 +1,15 @@
+public class TimT extends Member {
+    public TimT(String nama, String daerah) {
+        super(nama, daerah);
+    }
+
+    @Override
+    public String yel() {
+        return String.format("GO TEAM TI GO");
+    }
+    
+    @Override
+    public void show(){
+        super.show();
+    }
+}
