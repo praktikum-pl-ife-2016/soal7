c6476cc4c6e25dc5ecd8993d364581d40c552efa
FRDS <agung.frds7@gmail.com>
2016-05-04 08:07:16 +0700

    Upload source code

---
+++ src/T.java
@@ -0,0 +1,14 @@
+
+public class T extends Member {
+
+    public T(String nama, String daerah) {
+        super(nama, daerah);
+    }
+
+    @Override
+    public void yel() {
+        System.out.println("GO TEAM TI GO !");
+        System.out.println("=========================");
+    }
+
+}
