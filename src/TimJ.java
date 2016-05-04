8a9eebd7f7015462cd59b0d7335e03abfeb033da
luthfipratama <luthfip20.lp@gmail.com>
2016-05-04 07:39:44 +0700

    upload superclass

---
+++ src/TimJ.java
@@ -0,0 +1,10 @@
+public class TimJ extends Member {
+    
+    public TimJ(String nama, String daerah) {
+        super(nama, daerah);
+    }
+    
+    public void yelyel(){
+        System.out.println("WASSHOI J!");
+    }
+}
