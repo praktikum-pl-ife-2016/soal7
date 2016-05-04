de79f6f5908aad38da4d1b818ebb215b94cd1192
charlessangalangi <charles.sangga01@gmail.com>
2016-05-04 07:40:35 +0700

    source code

---
+++ src/TimK.java
@@ -0,0 +1,13 @@
+package praktikum7;
+public class TimK extends Member {
+        
+    public TimK(String nama, String daerahAsal){
+        super(nama, daerahAsal);
+    }
+
+    @Override
+    public void yel() {
+        System.out.println("WASSHOI K TEAM KEY !!!");
+        
+    }
+}
---
+++ src/TimT.java
@@ -0,0 +1,13 @@
+package praktikum7;
+public class TimT extends Member{
+        
+    public TimT(String nama, String daerahAsal){
+        super(nama, daerahAsal);
+    }
+
+    @Override
+    public void yel() {
+        System.out.println("GO TEAM TI GO");
+        
+    }
+}
