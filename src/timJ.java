b204face5d74ff33d45507642d6af6d33188649f
RiskaAndriani <andrianiriska17.ar@gmail.com>
2016-05-04 07:37:25 +0700

    source code

---
+++ src/timJ.java
@@ -0,0 +1,14 @@
+public class timJ extends MemberIdol {
+
+    public timJ(String name, String asal) {
+        super(name, asal);
+    }
+
+    public String yel() {
+        return String.format("WASSHOI J!");
+    }
+
+    public String show() {
+        return super.show();
+    }
+}
---
+++ src/timK.java
@@ -0,0 +1,14 @@
+public class timK extends MemberIdol {
+
+    public timK(String name, String asal) {
+        super(name, asal);
+    }
+
+    public String yel() {
+        return String.format("WASSHOI K TEAM KEY");
+    }
+
+    public String show() {
+        return super.show();
+    }
+}
