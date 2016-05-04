4e3b832266b8407360977cec9b73c924e3a7897c
afifdwhy <dwimurtadhaafif@yahoo.com>
2016-05-04 07:42:06 +0700

    upload TimK.java

---
+++ src/TimK.java
@@ -0,0 +1,8 @@
+public class TimK extends Member{
+    public TimK(String nama, String asal) {
+        super(nama, asal);
+    }
+    public void yel(){
+        System.out.println("WASSHOI K TEAM KEY\n");
+    }
+}
