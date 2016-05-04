c8fe7ea590dc017abb8dc2da3e7d29f284828b6c
Fikri <fikriihsan003@yahoo.com>
2016-05-04 08:01:54 +0700

    Menambahkan data diri,src,screenShoot

---
+++ src/TeamJ.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author user
+ */
+public  class TeamJ extends Member {
+
+    @Override
+    public String yel() {
+    return "WASSHOI j";
+    }
+    
+
+    
+    public  TeamJ(String nama,String asalDaerah){
+        super.nama=nama;
+        super.asalDaerah=asalDaerah;
+        
+    }
+    
+}
