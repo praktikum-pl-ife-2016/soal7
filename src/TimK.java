4648bbcfab3d779d1fd196660a04131a80844c8e
peitrae <peitrae@gmail.com>
2016-05-10 01:49:43 +0700

    Tambah file dan edit data

---
+++ src/TimK.java
@@ -0,0 +1,13 @@
+
+package jkt;
+
+public class TimK extends JKT {
+    public String yel(){
+     return "WASSHOI K TEAM KEY";   
+    }
+     public  TimK(String nama,String asal){
+        super.nama=nama;
+        super.asal=asal;
+    }
+}
+
